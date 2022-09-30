6342e243c9759a932253ae5525e5c0e92308ccf7
EricDebisch <eric.debisch@proton.me>
2022-09-30 02:01:44 +0200

    Update 2022-09-30: Functional color script

--- image_analysis.py
+++ image_analysis.py
@@ -7,16 +7,12 @@ from scipy.spatial import KDTree
 import webcolors
 import time
 
+#This function reads the RGB value for a specific pixel and returns the RGB value
 def getPixelRGB(funcPixelWidth, funcPixelLength):
-    #funcR, funcG, funcB = imageRGB.getpixel((funcPixelWidth,funcPixelLength))
     funcValueRGB = imageRGB.getpixel((funcPixelWidth,funcPixelLength))
-    #print(funcR, funcG, funcB)
-    #funcValueRGB = "(" + str(funcR)  + "," + str(funcG)  + "," + str(funcB) + ")"
-    #print(funcValueRGB)
     return funcValueRGB
-    
-    #csvPixelWriter.writerow([csvPixelCoordinate, csvValueRGB])
-    
+
+#This function receives the RGB value and converts it to the closets colorname according to the css3 definition
 def convert_rgb_to_names(func_rgb_tuple):
     # a dictionary of all the hex and their respective names in css3
     css3_db = webcolors.CSS3_HEX_TO_NAMES
@@ -33,74 +29,77 @@ def convert_rgb_to_names(func_rgb_tuple):
     #print(names[index])
     return names[index]
 
+#This function receives the colorname and the filepath to the metadata file and writes the colorname into the file, if the color is not already present in the file.
 def write_color_in_file(funcColorName, funcMetadataFileColorPath):
     duplicateColorCounter = 0
-    #firstValueCounter = 0
 
     funcMetadataColorsContent = open(funcMetadataFileColorPath, "r")
-    print(funcColorName)
-    for funcColorValue in funcMetadataColorsContent:
-        print(funcColorValue)
-        if(funcMetadataColorsContent.readline() in ["\n", "\r\n"]):
-            funcMetadataColorsContentWrite = open(funcMetadataFileColorPath, "a")
-            funcMetadataColorsContentWrite.write(funcColorName + "\n")
-            funcMetadataColorsContentWrite.close()
-            #firstValueCounter += 1
-            print("Hey, this is wrong")
-        elif(funcColorName == funcColorValue):
-            duplicateColorCounter = duplicateColorCounter + 1
-            print("LOL, duplicate")
+    funcMetadataColorsContentLength = len(funcMetadataColorsContent.readlines())
+    print(funcMetadataColorsContentLength)
+    print("colorName = " + funcColorName)
+
+    #funcListColorNames = []
+    #funcListColorNamesLength =
+
+    for funcColorValue in funcMetadataColorsContent: #Issue: The funcColorValue is currently not beeing read. The textfile cannot be read.
+        print("readit" + funcColorValue)
+        if(funcColorName == funcColorValue):
+            duplicateColorCounter  += 1
+            print("duplicate here")
         else:
             print("Color not on the list")
 
-    if(duplicateColorCounter == 0 and funcMetadataColorsContent.readline() != funcColorName):
+    if(duplicateColorCounter  == 0):
         funcMetadataColorsContentWrite = open(funcMetadataFileColorPath, "a")
         funcMetadataColorsContentWrite.write(funcColorName + "\n")
         funcMetadataColorsContentWrite.close()
     funcMetadataColorsContent.close()
     
 
-imagesPath = "C:/Users/Eric/Documents/FOM Studium/Bachelor-Thesis/Advertisement_Images/Web Crawling Images/images/fashion/H&M_Test"
-imagesPathFolders = os.listdir(imagesPath)
+listColorNames = []
+imagesPath = "C:/Users/Eric/Documents/FOM Studium/Bachelor-Thesis/Advertisement_Images/Web Crawling Images/images/fashion/H&M_Test" #Path to the folder to read all images - Adjustment (optional): Recursive to search in subfolders too
+imagesPathFolders = os.listdir(imagesPath) #Lists the imagefile names in the specified folder
 
+#For loop to go through every imagefile to read the pixels
 for imageFileName in imagesPathFolders:
     print(imageFileName)
-    imageFullPath = imagesPath + "/" + imageFileName
+    imageFullPath = imagesPath + "/" + imageFileName #Creates the full pathname to the image file
     print(imageFullPath)
-    metadataFileColorPath = imagesPath + "/" + "metadata_color_" + imageFileName.replace(".jpg","") + ".txt"
-    #open(metadataFileColorPath, "w")
-    MetadataColorsContent = open(metadataFileColorPath, "a")
-    #time.sleep(5)
-    #MetadataColorsContent.write("colorName;\n")
-    MetadataColorsContent.close()
-    image =  Image.open(imageFullPath)
-    imageRGB = image.convert('RGB')
+    metadataFileColorPath = imagesPath + "/" + "metadata_color_" + imageFileName.replace(".jpg","") + ".txt" #Creates the full path to the metadata_color file for the specific image
+    MetadataColorsContent = open(metadataFileColorPath, "a") #Creates a metadata textfile
+    MetadataColorsContent.close() #Closing the textfile. Will be opened later
+    image =  Image.open(imageFullPath) #Opens the imagefile with all information like, image resolution
+    imageRGB = image.convert('RGB') #Converts the imgage pixels to the RGB value
+
+    #Two loop that through each pixel. The first for loop for the width, the secend for loop for the length. Both parameters combined result to a specific pixel on the image
     for counterWidth in range(0, image.size[0], 1):
         for counterLength in range(0, image.size[1], 1):
-            valueRGB = getPixelRGB(counterWidth, counterLength)
-            colorName = convert_rgb_to_names(valueRGB)
-            write_color_in_file(colorName, metadataFileColorPath)
-            #print(counterLength, counterWidth)
+            valueRGB = getPixelRGB(counterWidth, counterLength) #Opens the function to read the RGB value for the pixel
+            colorName = convert_rgb_to_names(valueRGB) #Opens the function to convert the rgb value to a colorname that is the closest to the RGB value
+            duplicateColorCounter = 0
+            print(counterWidth, counterLength)
+            listColorNamesLength = len(listColorNames)
+           #print(listColorNamesLength)
+            if(listColorNamesLength == 0):
+               listColorNames.append(colorName) #Issue: Second textfile is empty and does not contain any colors. Please test with smaller image size. Process time is 17 seconds for 10 x 1000 pixels
+               metadataColorsContentWrite = open(metadataFileColorPath, "a")
+               metadataColorsContentWrite.write(colorName + "\n")
+               metadataColorsContentWrite.close()
+            else:               
+                for colorNameValue in listColorNames:
+                    #print("value = " + colorNameValue)
+                    #print("name = " + colorName)
+                    if(colorName == colorNameValue):
+                        duplicateColorCounter  += 1
+                        #print("duplicate here")
+
+                if(duplicateColorCounter  == 0):
+                    #print("write in file")
+                    listColorNames.append(colorName)
+                    metadataColorsContentWrite = open(metadataFileColorPath, "a")
+                    metadataColorsContentWrite.write(colorName + "\n")
+                    metadataColorsContentWrite.close()
+                #print(duplicateColorCounter)
 
 
-#csvPixelPath = "C:/Users/Eric/Documents/FOM Studium/Bachelor-Thesis/PixelDistribution.csv"
-#csvPixelWriter = csv.writer(open(csvPixelPath, "w"))
-#csvPixelWriter.writerow(["PixelCoordinate", "ValueRGB"])
-
-
-        
-        
-
-#image.show()
-
-
-#info_dict = {
-#    "Filename": image.filename,
-#    "Image Size": image.size,
-#    "Image Height": image.height,
-#    "Image Width": image.width,
-#    "Image Format": image.format,
-#    "Image Mode": image.mode,
-#    "Image is Animated": getattr(image, "is_animated", False),
-#    "Frames in Image": getattr(image, "n_frames", 1)
-#}
+            #write_color_in_file(colorName, metadataFileColorPath) #Opens the function to write the colorname into the metadata file
