693da0a500871eb1f85829605ef94fd4b7398520
EricDebisch <eric.debisch@proton.me>
2022-10-10 00:29:57 +0200

    Update 2022-10-10: Adjusted match check script

--- match_check_metadata.py
+++ match_check_metadata.py
@@ -1,13 +1,16 @@
 import os
 import re
 
-
-sourceImagePath = "C:/Users/Eric/Documents/FOM Studium/Bachelor-Thesis/Advertisement_Images/Web Crawling Images/images/fashion/H&M/18900.jpg"
+sourceImagePath = "C:/Users/Eric/Documents/FOM Studium/Bachelor-Thesis/Advertisement_Images/Web Crawling Images/images/fashion/H&M/18894.jpg"
 sourceImageMetadataObjectsPath = sourceImagePath.replace(".jpg","_metadata_objects.txt"  )
 sourceImageMetadataColorsPath = sourceImagePath.replace(".jpg","_metadata_color.txt"  )
 
+matchingPercentageThreshhold = 0.5
+
 listColorNames = [] #List for colornames, that are in the metadata file
 listImagesInFolder = [] #List for only jpg or jpeg files in the target foler. All other file types will be ignored
+listInputTextForPegasus = []
+listInputTextForBLEURT = []
 regexImageFile = ".+\.jpe?g" #Reguluar expression for a file name with .jpg or jpeg file extension
 imagesPath = "C:/Users/Eric/Documents/FOM Studium/Bachelor-Thesis/Advertisement_Images/Web Crawling Images/images/fashion/H&M" #Path to the folder to read all images - Adjustment (optional): Recursive to search in subfolders too
 imagesPathFolders = os.listdir(imagesPath) #Lists the imagefile names in the specified folder
@@ -19,7 +22,8 @@ for imagePathFolder in imagesPathFolders:
 
 for imageFileName in listImagesInFolder:
     matchCounterObject = 0
-    #print(imageFileName)
+    atchObjectPercentage = 0
+    print("image Name: " + str(imageFileName))
     imageFullPath = imagesPath + "/" + imageFileName #Creates the full pathname to the image file
     #print(imageFullPath)
     metadataFileColorPath = imagesPath + "/" +  imageFileName.replace(".jpg","") + "_metadata_color" + ".txt" #Creates the full path to the metadata_color file for the specific image
@@ -31,13 +35,14 @@ for imageFileName in listImagesInFolder:
     referenceMetadataObjects = open(metadataFileObjectsPath, "r")
     referenceMetadataColor = open(metadataFileColorPath, "r")
 
-    for lineSourceObject in sourceImageMetadataObjects:
-        
+    for lineSourceObject in sourceImageMetadataObjects:        
         for lineReferenceObject in referenceMetadataObjects:
-            #print(lineReferenceObject)
+            print("Source Object: " + str(lineSourceObject))
+            print("Reference Object: " + str(lineReferenceObject))
             if(lineSourceObject == lineReferenceObject):
                 matchCounterObject += 1
                 print("Match found")
+                print(matchCounterObject)
                 break
             else:
                 i = 0
@@ -46,7 +51,6 @@ for imageFileName in listImagesInFolder:
     referenceMetadataObjectsLengthRead = open(metadataFileObjectsPath, "r").readlines()
     referenceMetadataObjectsLength = len(referenceMetadataObjectsLengthRead)
     #print(referenceMetadataObjectsLength)
-    print("Source Object")
     if(referenceMetadataObjectsLength > 0):
         matchObjectPercentage = matchCounterObject / referenceMetadataObjectsLength
     else:
@@ -54,11 +58,22 @@ for imageFileName in listImagesInFolder:
     
     print("Matching Percentage is: " + str(matchObjectPercentage) )
     
+    metadataFileZeroShotTextPath = imagesPath + "/" + imageFileName.replace(".jpg","") + "_metadata_zero-shot_text.txt"
+    metadataFileZeroShotText = open(metadataFileZeroShotTextPath, "r")
+
+    if(matchObjectPercentage >= matchingPercentageThreshhold):
+        listInputTextForPegasus.append(metadataFileZeroShotText.readline())
+
+    else:
+        listInputTextForBLEURT.append(metadataFileZeroShotText.readline())
+    
+
     sourceImageMetadataObjects.close()
     sourceImageMetadataColor.close()
     referenceMetadataObjects.close()
     referenceMetadataColor.close()
-    #referenceMetadataObjectsLengthRead.close()
+    metadataFileZeroShotText.close()
+    
 
 
 
