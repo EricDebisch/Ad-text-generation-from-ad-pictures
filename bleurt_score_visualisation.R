c9de61ca9798ec467b0f99504e50ca4d9ab0c9c8
EricDebisch <eric.debisch@proton.me>
2022-10-27 22:36:37 +0200

    Update 2022-10-27: Adjusted R analysis

--- bleurt_score_visualisation.R
+++ bleurt_score_visualisation.R
@@ -3,8 +3,9 @@ library(mosaic)
 library(psych)
 library(readxl)
 
-bluert_scores <- read.csv2("C:\\Users\\Eric\\Documents\\FOM Studium\\Bachelor-Thesis\\Advertisement_Images\\Web Crawling Images\\images\\Testrun_00_automobile_finished\\evaluation_representation\\BLEURT_scores_automobile.csv", header = FALSE, sep = ";")
-View(bluert_scores)
+bluert_scores_file <- read.csv2("C:\\Users\\Eric\\Documents\\FOM Studium\\Bachelor-Thesis\\Advertisement_Images\\Web Crawling Images\\images\\Testrun_00_automobile_finished\\evaluation_representation\\BLEURT_scores_automobile.csv", header = TRUE, sep = ";")
+View(bluert_scores_file)
+str(bluert_scores_file)
 
-boxplot(bluert_scores)
-mean(bluert_scores)
+gf_boxplot( bluert_score_values ~ 2, data = bluert_scores_file, xlab = element_blank())
+gf_point(bluert_scores_file ~ score_id, data = bluert_scores_file)
